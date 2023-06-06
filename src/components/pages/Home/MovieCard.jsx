c9b0097fc151b10e86f5b6204b50abe62f15270f
unknown <irfanulamin07@gmail.comm>
2023-06-06 19:20:56 +0600

    card designs has been added

--- src/components/pages/Home/MovieCard.jsx
+++ src/components/pages/Home/MovieCard.jsx
@@ -1,14 +1,41 @@
 import React from "react";
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
 
 const MovieCard = ({ movie }) => {
   const { show } = movie;
+  const notagSummary = show?.summary?.replace(/<\/?[^>]+(>|$)/g, "");
+  const twentywordsSummary = notagSummary.substring(0, 80);
   return (
-    <div className="bg-white border flex ">
-      <img src={show?.image?.original} alt="Movie" className="w-52 h-64" />
-      <div className="p-12">
-        <h2 className="text-2xl font-semibold text-right tracking-wide">
-          {show?.name}
-        </h2>
+    <div className="bg-white border border-slate-800 flex items-center">
+      <div>
+        <img
+          src={show?.image?.original}
+          alt="Movie"
+          className="w-64 h-72 object-cover"
+        />
+      </div>
+      <div className="p-4 pl-7">
+        <h2 className="text-4xl font-semibold  tracking-wide">{show?.name}</h2>
+        {show?.rating?.average ? (
+          <Rating
+            style={{ maxWidth: 150 }}
+            readOnly
+            value={Math.round(show?.rating?.average / 2)}
+          />
+        ) : (
+          <p className="text-red-700">No Ratings</p>
+        )}
+        <div className="flex items-center py-2 gap-2">
+          {show?.genres.map((genre) => (
+            <div className="badge badge-outline ">{genre}</div>
+          ))}
+        </div>
+        <p className="text-sm">{twentywordsSummary}....</p>
+        <button className="btn w-1/2 mt-4  bg-red-700 text-white hover:bg-white hover:text-red-700 transation_custom border-none rounded-sm">
+          Show Details
+        </button>
       </div>
     </div>
   );
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 py-24 bg-yellow-600 text-base-content">
+    <footer className="footer p-10 py-24 bg-white text-base-content">
       <div>
         <img src="/logo.jpg" className="h-16 w-16" alt="logo" />
-        <p className="font-semibold text-2xl text-white">Theater Maze</p>
+        <p className="font-semibold text-2xl text-black">Theater Maze</p>
       </div>
       <div>
-        <span className="text-lg font-bold text-white">Company</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
+        <span className="text-lg font-bold text-black">Company</span>
+        <a className="link link-hover text-black">About us</a>
+        <a className="link link-hover text-black">Contact</a>
+        <a className="link link-hover text-black">Jobs</a>
       </div>
       <div>
-        <span className="text-lg font-bold text-white">Legal</span>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <span className="text-lg font-bold text-black">Legal</span>
+        <a className="link link-hover text-black">Terms of use</a>
+        <a className="link link-hover text-black">Privacy policy</a>
+        <a className="link link-hover text-black">Cookie policy</a>
       </div>
       <div>
-        <span className="text-lg font-bold text-white">Contact us</span>
-        <a className="link link-hover">theater@maze.com</a>
-        <a className="link link-hover">+123 4567 8910</a>
+        <span className="text-lg font-bold text-black">Contact us</span>
+        <a className="link link-hover text-black">theater@maze.com</a>
+        <a className="link link-hover text-black">+123 4567 8910</a>
       </div>
     </footer>
   );
